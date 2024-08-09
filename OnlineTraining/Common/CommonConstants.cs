04377ce7528db3b0e6bf3fd7c0ab7bf5af0451dc
Tran Thanh Dat <tthanhdat.pb@gmail.com>
2024-08-09 23:13:36 +0700

    Add CommonContants

---
+++ OnlineTraining/Common/CommonConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineTraining.Common
+{
+    public static class CommonConstants
+    {
+        public static string USER_SESSION = "USER_SESSION";
+        public static string CurentCulture  { get; set; }
+    }
+}
