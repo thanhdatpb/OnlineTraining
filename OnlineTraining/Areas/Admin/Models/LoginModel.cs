da34ce1c79bf6b817448b50ad8b1b62e5ee9f86d
Tran Thanh Dat <tthanhdat.pb@gmail.com>
2024-08-09 23:12:53 +0700

    Add LoginModel for Admin

---
+++ OnlineTraining/Areas/Admin/Models/LoginModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineTraining.Areas.Admin.Models
+{
+    public class LoginModel
+    {
+    }
+}
