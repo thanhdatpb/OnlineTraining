65a579d69e7c5388f1f4fffb2fd01c8df7bcfbce
Tran Thanh Dat <tthanhdat.pb@gmail.com>
2024-08-09 21:23:11 +0700

    Update Login  UserDao

--- Model/Dao/UserDao.cs
+++ Model/Dao/UserDao.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,12 +13,16 @@ namespace Model.Dao
         public UserDao() { 
             db = new OnlineTrainingEntities();
         }
+
+        //Hàm thêm dữ liệu
         public long Insert(User entity)
         {
             db.Users.Add(entity);
             db.SaveChanges();
             return entity.ID;
         }
+
+        //Hàm cập nhật, sửa dữ liệu
         public bool Update(User entity)
         {
             try
@@ -43,15 +48,91 @@ namespace Model.Dao
                 return false;
             }
         }
+
+        //Hàm xóa dữ liệu
         public bool Delete(int id)
         {
             try
             {
+                var user = db.Users.Find(id);
+                db.Users.Remove(user);
+                db.SaveChanges();
                 return true;
             }
             catch(Exception)
             { 
                 return false; 
+            }
+        }
+
+        public User GetByUserName(string userName)
+        {
+            return db.Users.SingleOrDefault(x => x.UserName == userName);
+        }
+
+        //Hàm cho chức năng show danh sách người dùng(bao gồm phân trang)
+        //Chức năng dành cho admin, kiểm tra có bao nhiêu tk
+        public IEnumerable<User> ListAllPaging(string searchString, int page, int pagesize)
+        {
+            IQueryable<User> model = db.Users;
+            model = model.Where(x => x.UserName == "admin");
+       
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = model.Where(x => x.UserName.Contains(searchString) || x.Name.Contains(searchString));
+            }
+            return model.OrderByDescending(x => x.CreatDate).ToPagedList(page, pagesize);
+        }
+
+        public User ViewDetail(int id) {
+            return db.Users.Find(id);  
+        }
+
+        public int Login(string userName, string password, bool isLoginAdmin = false)
+        {
+            var result = db.Users.SingleOrDefault(x => x.UserName == userName);
+            if (result == null)
+            {
+                return 0;
+            }
+            else
+            {
+                if (isLoginAdmin == true)
+                {
+                    if (result.Status == false)
+                    {
+                        return -1;
+                    }
+                    else
+                    {
+                        if (result.Password == password)
+                        {
+                            return 1;
+                        }
+                        else
+                        {
+                            return -2;
+                        }
+                    } 
+                }
+                else
+                {
+                    if (result.Status == false)
+                    {
+                        return -1;
+                    }
+                    else
+                    {
+                        if (result.Password == password)
+                        {
+                            return 1;
+                        }
+                        else
+                        {
+                            return -2;
+                        }
+                    }
+                }
             }
         }
     }
