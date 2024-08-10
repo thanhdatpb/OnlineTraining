a43e5b60ff632820e18dcd6aefc40ecde36bfc5b
Tran Thanh Dat <tthanhdat.pb@gmail.com>
2024-08-11 04:45:39 +0700

    LoginModel

--- OnlineTraining/Areas/Admin/Models/LoginModel.cs
+++ OnlineTraining/Areas/Admin/Models/LoginModel.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace OnlineTraining.Areas.Admin.Models
 {
     public class LoginModel
     {
+        [Required(ErrorMessage ="Mởi bạn nhập UserName")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Mởi bạn nhập PassWord")]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+
     }
 }
