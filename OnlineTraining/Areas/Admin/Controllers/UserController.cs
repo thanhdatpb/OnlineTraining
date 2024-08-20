0a3ae53654aabb9a14f5f046803f7dbacacd79f8
Tran Thanh Dat <tthanhdat.pb@gmail.com>
2024-08-20 20:08:10 +0700

    Add UserController

---
+++ OnlineTraining/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace OnlineTraining.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: Admin/User
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
